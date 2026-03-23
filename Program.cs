1a2f2b3afb47f973fe7dfdf5aab15ef88d354bd1
Casper Gamborg <Gamborgc@gmail.com>
2026-03-23 06:26:48 +0100

    Small cleanup
    Removed commented-out TODO notes from Program.cs.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,3 @@ class Program
         programStart.StartProgram();
     }
 }
-
-/*
-todo 
-    er MaxMødeTid en metode? er det ikke smartere at manupulere den fra metoden, og ikke inde i factory? 
-    Hvordan bruges polymorphi, hvis det overhovedet er det? (kan se, at jeg bruger override)    
-    Snak med Niels om: 
-    Medarbejder factory logik? 
-    MaxMødeTid 
-    Validator - hvorfor laver vi det egenligt til et array? 
-
- */
